b7b9faa5c391cf42c2c7f550b88b8bc8eafe97be
Fazna-kozhipparambil <103088057+Fazna-kozhipparambil@users.noreply.github.com>
2024-08-21 14:50:16 -0500

    Update global_temperature_visualization_dashboard.py

--- global_temperature_visualization_dashboard.py
+++ global_temperature_visualization_dashboard.py
@@ -298,8 +298,6 @@ Effect Size: The large magnitude of the t-statistic suggests a substantial diffe
 1. Set Up Your Environment in Colab
 """
 
-!pip install streamlit 
-
 """2. Write  Streamlit App Code"""
 
 # Commented out IPython magic to ensure Python compatibility.
